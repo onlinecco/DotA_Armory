0bd4faa07802548b82bfe82b2264e2b5507be884
Joey Cao <joeycao@wirelessprvnat-172-16-149-201.near.illinois.edu>
2014-02-26 13:45:43 -0600

    weq

---
+++ view/header.php
@@ -0,0 +1,23 @@
+<div class="header">
+<ul>
+				<li class="selected">
+					<a href="heroes.html"><span>H</span>eroes</a>
+				</li>
+				<li>
+					<a href="items.html"><span>I</span>tems</a>
+				</li>
+				<li>
+					<a href="players.html"><span>P</span>layers</a>
+				</li>
+				<li>
+					<a href="matches.html"><span>M</span>atches</a>
+				</li>
+				<li style="margin: 0 0 0 800px">
+					<a href="login.php"><span>L</span>ogin</a>
+				</li>
+				<li>
+					<a href="register.php"><span>R</span>egsiter</a>
+
+				</li>
+			</ul>
+</div>
