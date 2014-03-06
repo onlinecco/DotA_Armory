ecc17949e01b1762d6508c96e4884dc4891eb56c
web requester <dotaarmory@engr-cpanel1.engr.illinois.edu>
2014-03-06 15:22:59 -0600

    aha

--- back/steamopenid.php
+++ back/steamopenid.php
@@ -1,22 +1,15 @@
 <?php
-require 'openid.php';
+require "class/openid.php";
 try {
     # Change 'localhost' to your domain name.
     $openid = new LightOpenID('engr-cpanel-mysql.engr.illinois.edu');
     if(!$openid->mode) {
-        if(isset($_POST['openid_identifier'])) {
-            $openid->identity = $_POST['openid_identifier'];
+            $openid->identity = "http://steamcommunity.com/openid";
             # The following two lines request email, full name, and a nickname
             # from the provider. Remove them if you don't need that data.
            // $openid->required = array('contact/email');
             //$openid->optional = array('namePerson', 'namePerson/friendly');
             header('Location: ' . $openid->authUrl());
-        }
-?>
-<form action="" method="post">
-    OpenID: <input type="text" name="openid_identifier" /> <button>Submit</button>
-</form>
-<?php
     } elseif($openid->mode == 'cancel') {
         echo 'User has canceled authentication!';
     } else {
